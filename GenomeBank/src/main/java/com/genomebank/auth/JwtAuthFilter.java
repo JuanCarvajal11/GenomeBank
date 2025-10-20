371980b8f4cf080a3918411a9a8ae30523ae8349
JuanCarvajal11 <carvajaljuanmanuel8@gmail.com>
2025-10-20 18:20:03 -0500

    Se agrego una función para completar que exista un token activo

--- GenomeBank/src/main/java/com/genomebank/auth/JwtAuthFilter.java
+++ GenomeBank/src/main/java/com/genomebank/auth/JwtAuthFilter.java
@@ -2,7 +2,6 @@ package com.genomebank.auth;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
