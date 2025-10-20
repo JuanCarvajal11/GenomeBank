02ec8b300a6dff7737f6774683e4195c28bc6448
JuanCarvajal11 <carvajaljuanmanuel8@gmail.com>
2025-10-20 18:19:04 -0500

    Se agrego una función para completar que exista un token activo

--- GenomeBank/src/main/java/com/genomebank/auth/JwtAuthFilter.java
+++ GenomeBank/src/main/java/com/genomebank/auth/JwtAuthFilter.java
@@ -26,6 +26,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         String header = request.getHeader("Authorization");
         if(header == null || !header.startsWith("Bearer ")){
             filterChain.doFilter(request, response);
+            return;
         }
 
         String token = header.substring(7);
