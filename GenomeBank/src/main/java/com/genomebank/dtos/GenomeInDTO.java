8408d049b88b0210de42f2111cf4bd1246676ddf
Juan Manuel Atehortua <jatehortua2006@gmail.com>
2025-10-20 18:02:39 -0500

    Configuración externa

--- GenomeBank/src/main/java/com/genomebank/dtos/GenomeInDTO.java
+++ GenomeBank/src/main/java/com/genomebank/dtos/GenomeInDTO.java
@@ -10,20 +10,4 @@ import lombok.NoArgsConstructor;
 public class GenomeInDTO {
     private String versionName;
     private Long speciesId;
-
-    public String getVersionName() {
-        return versionName;
-    }
-
-    public void setVersionName(String versionName) {
-        this.versionName = versionName;
-    }
-
-    public Long getSpeciesId() {
-        return speciesId;
-    }
-
-    public void setSpeciesId(Long speciesId) {
-        this.speciesId = speciesId;
-    }
 }
